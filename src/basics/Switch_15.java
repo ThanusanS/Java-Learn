d5dce82246d297beecbd8804226e08b1006653ef
ThanusanS <thanusan0411@gmail.com>
2026-02-10 08:36:48 +0530

    Added File

--- src/basics/Switch_15.java
+++ src/basics/Switch_15.java
@@ -30,5 +30,24 @@ class Switch_15 {
 
              
         }
+
+
+            int choice = 1;
+
+            // 1 = Coffee
+            // 2 = Tea
+
+            switch (choice) {
+            case 1:
+                System.out.println("You ordered Coffee");
+                break;
+
+            case 2:
+                System.out.println("You ordered Tea");
+                break;
+
+            default:
+                System.out.println("Invalid choice");
+            }
     }
 }
