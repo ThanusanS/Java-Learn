4f05d8edacc48069d1bc87996b7dd08c9dd28cdb
ThanusanS <thanusan0411@gmail.com>
2026-02-11 09:46:41 +0530

    Added File

---
+++ src/basics/WhileLoop_16.java
@@ -0,0 +1,23 @@
+class WhileLoop_16 {
+    public static void main(String[] args) {
+        int countdown = 3;
+
+        while (countdown > 0) {
+        System.out.println(countdown);
+        countdown--;
+        }
+
+        System.out.println("Happy New Year!!");
+
+
+
+
+            int i = 0;
+            while (i < 5) {
+            System.out.println(i);
+            i++;
+            }
+
+
+    }
+}
