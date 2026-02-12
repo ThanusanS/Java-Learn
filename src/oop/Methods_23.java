3ee2eaf034178d8a74d11870783e5ce8294b6852
ThanusanS <thanusan0411@gmail.com>
2026-02-12 20:46:30 +0530

    Added File

---
+++ src/oop/Methods_23.java
@@ -0,0 +1,27 @@
+public class Methods_23{
+
+
+    // Method / Function Creating
+
+    void greeting (){
+
+        for (int i=0; i<5 ; i++){
+           System.out.println("Vanakkam");
+        }    
+    }
+
+
+
+
+
+
+    // Method/Function Calling
+
+    public static void main (String[] args){
+
+        Methods_23 obj1 = new Methods_23();  // Create Object (obj1)
+        obj1.greeting();
+        obj1.greeting();
+        obj1.greeting();
+    }
+}
