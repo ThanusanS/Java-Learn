7fc664493cb6b519ae71a74c58ee85a872ff68a0
ThanusanS <thanusan0411@gmail.com>
2026-02-12 20:15:14 +0530

    Added File

--- src/oop/ClassesAndObjects_22.java
+++ src/oop/ClassesAndObjects_22.java
@@ -1,1 +1,27 @@
-hhh
+public class ClassesAndObjects_22{
+    int price=0;
+
+    public static void main(String[] args){
+
+
+        ClassesAndObjects_22  lap1 = new ClassesAndObjects_22();  // Create an Object
+
+        lap1.price=40000;
+
+        System.out.println(lap1.price);
+
+
+
+
+       // Create Another Object
+
+
+        ClassesAndObjects_22  lap2 = new ClassesAndObjects_22();
+
+        lap2.price=70000;
+
+        System.out.println(lap2.price);
+
+        
+    }
+}
