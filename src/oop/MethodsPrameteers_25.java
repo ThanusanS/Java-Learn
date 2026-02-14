3ed31ddfdc54100a6b30c212c8ef0d85dbb86088
ThanusanS <thanusan0411@gmail.com>
2026-02-14 12:35:33 +0530

    Added File

---
+++ src/oop/MethodsPrameteers_25.java
@@ -0,0 +1,13 @@
+public class MethodsPrameteers_25{
+
+
+    static int sum(int a , int b){
+        return a+b;
+    }
+
+
+    public static void main(String[] args){
+        int result = sum (6,9);
+        System.out.println(result);
+    }
+}
