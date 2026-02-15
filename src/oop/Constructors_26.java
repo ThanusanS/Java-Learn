cbc8dde15bd697f944b859082dd358948cc55343
ThanusanS <thanusan0411@gmail.com>
2026-02-15 08:39:54 +0530

    Added File

--- src/oop/Constructors_26.java
+++ src/oop/Constructors_26.java
@@ -1,5 +1,20 @@
-public class Constructors_26 {
-    public static  void main (String[] args){
-        System.out.println("Hello");
+     // Create a Main class
+    public class Constructors_26 {
+        int x;  // Create a class attribute
+
+        // Create a class constructor for the Main class
+        public Constructors_26() {
+            x = 5;  // Set the initial value for the class attribute x
+        }
+
+        public static void main(String[] args) {
+            Constructors_26 myObj = new Constructors_26(); // Create an object of class Main (This will call the constructor)
+            System.out.println(myObj.x); // Print the value of x
+        }
     }
-}
+
+    // Outputs 5
+
+
+
+
